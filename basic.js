9840c422baa8509f534efbea25a7bda27a3d79d0
anikmondol <anikmondol558363@gmil.com>
2024-04-25 16:32:28 +0600

    basic file added

--- basic.js
+++ basic.js
@@ -1,1 +1,48 @@
-// 
+// // primitive datatype
+
+// const x = 2;
+// const y = true;
+// const z = 'hello';
+
+
+// let i = x;
+// i = 10;
+
+
+// console.log(`x = ${x}`);
+// console.log(`i = ${i}`);
+
+
+
+// // non primitive datatype
+// const a = {name : 'anik'};
+// const num = [11, 51];
+
+
+// const num1 = num;
+// num1.push(120);
+
+// console.log(`num is =`, num);
+// console.log(`num1 is =`, num1);
+
+
+// const c = a;
+// c.job = `study`;
+
+// console.log(`c is =`, c);
+// console.log(`a is =`, a);
+
+
+
+// ----------> truthy and false value
+
+
+// truthy value --------> true, 10, -1, [], {}, string, other then all value
+
+
+// false value-------==> false, 0, '', NaN, null, undefined
+
+
+//------>check true and false vale
+// console.log(!!value);
+
