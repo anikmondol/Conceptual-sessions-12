4e5fae46fbd19135309a36583529839876f03e33
anikmondol <anikmondol558363@gmil.com>
2024-04-25 19:59:37 +0600

    added class file in this projects

---
+++ advence/class.js
@@ -0,0 +1,38 @@
+// class Animal{
+//     name = 'abc';
+//     color = 'black';
+
+//     info(){
+//         console.log(`my animal name is ${this.name}`);
+//     }
+
+
+// }
+
+// const cow = new Animal();
+// console.log(cow);
+// console.log(cow.color);
+
+// cow.info()
+
+// -----------------------------
+
+
+class Animal{
+    constructor(AnimalName, AnimalColor){
+        this.name = AnimalName;
+        this.color = AnimalColor;
+    }
+    showInfo(){
+        console.log(`my pet name is ${this,this.name}`);
+    }
+}
+
+const dog = new Animal('a', 'c');
+const cat = new Animal('d', 'e');
+
+
+console.log(cat);
+console.log(dog);
+
+cat.showInfo();
