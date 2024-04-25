1008e52255cdeeccdeeff2af49d6d1bc16ac0acb
anikmondol <anikmondol558363@gmil.com>
2024-04-25 18:47:47 +0600

    added indexing in projects file

---
+++ indexing.js
@@ -0,0 +1,6 @@
+const number = [10, 21, 30, 41, 50];
+
+number.map((item, index, array) =>{
+    // console.log(`item :`, item, `index :`, index, `array :`, array);
+    console.log(`item :`, item, `index :`, index);
+})
