6e1024f53617d27be0cdb54656af46f0d17fdc8c
anikmondol <anikmondol558363@gmil.com>
2024-04-25 17:31:03 +0600

    added arrow function file

---
+++ arrow.js
@@ -0,0 +1,26 @@
+// function add(a, b){
+//     return a + b;
+// }
+
+// console.log(add(2, 2));
+
+
+// -------------------
+
+const add = (a, b) => a + b;
+
+// console.log(add(10, 20));
+
+
+// --------------------
+
+const double = a => a * 2;
+
+console.log(double(2));
+
+
+// -------------------------
+
+const pI = () => Math.PI;
+
+console.log(pI());
